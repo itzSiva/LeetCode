2a576b9597ae435f92a0940a6e2cacb64e2a0976
Siva V <99382701+itzSiva@users.noreply.github.com>
2024-06-20 16:19:00 +0530

    Time: 0 ms (100%), Space: 40.1 MB (74.82%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,7 +1,14 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        String n1=Integer.toString(9,3);
-        System.out.println(n1);
+        String str=String.valueOf(n);
+        for(int j=2;j<=n-2;j++){
+            String ans=Integer.toString(Integer.parseInt(str,10),j);
+            for(int i=0;i<ans.length();i++){
+                if(ans.charAt(i)!=ans.charAt(ans.length()-i-1)){
+                    return false;
+                }
+            }
+        }
         return false;
     }
 }
