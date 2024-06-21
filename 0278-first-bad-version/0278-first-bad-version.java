4d118fd4c43bdb3e305f991d7380b71fe60e3721
Siva V <99382701+itzSiva@users.noreply.github.com>
2024-06-21 15:26:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,12 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+     for(int i=1;i<n;i++){
+        if(isBadVersion(i))
+        return i;
+     } 
+     return n;  
+    }
+}
